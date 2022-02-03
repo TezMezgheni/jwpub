f7b527f05aced985c6547fca67732db75de3854b
Tez <moatez2311@gmail.com>
2022-02-03 09:07:46 +0100

    first commit

---
+++ src/main/java/tez/projects/jwpub/JwpubApplication.java
@@ -0,0 +1,13 @@
+package tez.projects.jwpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tez/projects/jwpub/JwpubApplicationTests.java
@@ -0,0 +1,13 @@
+package tez.projects.jwpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwpubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
